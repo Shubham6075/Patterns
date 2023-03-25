4e8a24a91a4c928338cb9272232e2dc00d8f112d
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-25 09:03:56 +0530

    Add files via upload

---
+++ p8.java
@@ -0,0 +1,23 @@
+public class p8 {
+	public static void main(String [] args) {
+		/*
+		       1
+		       2 3
+		       4 5 6
+		       7 8 9 10
+		       11 12 13 14 15
+
+		 */
+		int n = 5;
+		int number = 1;
+		// outer loop : row
+		for(int i=1; i<=n; i++) {
+			// inner loop : column
+			for(int j=1; j<=i; j++) {
+				System.out.print(number+" ");
+				number++;
+			}
+			System.out.println();
+		}
+	}
+}
